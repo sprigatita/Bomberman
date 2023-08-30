162a12790c2784e35aac7083f1346eacc327aaec
Marta <randazzomarta@icloud.com>
2023-08-30 17:15:21 +0200

    Layout implementation

--- src/View/FinestraDiGioco.java
+++ src/View/FinestraDiGioco.java
@@ -1,20 +1,66 @@
 package View;
-import javax.swing.JFrame;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 
 
 public class FinestraDiGioco extends JFrame {
 
-    public FinestraDiGioco () {
-    	this.init();
+    public FinestraDiGioco() {
+        this.init();
     }
-    
+
     public void init() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setTitle("JBomberman - Mignola col Prof");
-        this.setLocationRelativeTo(null);
-        
-    }
-    
+        this.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS)); // Layout verticale
 
+        // Crea il pannello delle informazioni
+        JPanel infoPanel = createInfoPanel();
+
+        // Crea il GamePanel
+        GamePanel gamePanel = new GamePanel();
+
+        // Aggiungi il pannello delle informazioni sopra al GamePanel
+        this.add(infoPanel);
+        this.add(gamePanel);
+
+        this.pack();
+        this.setLocationRelativeTo(null);
+    }
+
+    private JPanel createInfoPanel() {
+        JPanel infoPanel = new JPanel();
+        
+
+
+        // Scelta del layout orizzontale
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.X_AXIS));
+
+        /* 
+         * Qui si aggiungono le componenti tipo informazioni che ci servono a schermo,
+         * noi le dovremo prendere dalla classe user quindi magari serve istanziare questo 
+         * panel altrove.
+         */
+        JLabel usernameLabel = new JLabel("Username: ");
+        JLabel scoreLabel = new JLabel("Score: ");
+
+        infoPanel.add(usernameLabel);
+        infoPanel.add(scoreLabel);
+
+
+        return infoPanel;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            FinestraDiGioco finestra = new FinestraDiGioco();
+            finestra.setVisible(true);
+        });
+    }
 }
